ba515af08ba2c725f72df298c41ffc53214ea7b8
Denys <denisbadakaty51@gmail.com>
2024-04-14 19:25:19 +0300

    update scss loader

--- config/builder/loaders/index.ts
+++ config/builder/loaders/index.ts
@@ -3,20 +3,23 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { RuleSetRule } from 'webpack';
 
 export const buildLoaders = ({ isDev }: BuildOptions): RuleSetRule[] => {
-  const typescriptLoader = {
+  const typescriptLoader: RuleSetRule = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
 
-  const scssLoader = {
+  const scssLoader: RuleSetRule = {
     test: /\.s[ac]ss$/i,
     use: [
       isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       {
         loader: 'css-loader',
         options: {
-          modules: true,
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+          },
         },
       },
       'sass-loader',
