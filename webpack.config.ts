8728a3a195cbd172bec0be7e009e8fa44b85ae30
Denys <denisbadakaty51@gmail.com>
2024-04-14 16:52:42 +0300

    update field name

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,6 @@
-import { Configuration } from 'webpack';
-import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
 import path from 'path';
+import { Configuration } from 'webpack';
+import { BuildEnv, BuildPaths, buildWebpackConfig } from './config';
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
