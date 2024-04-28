37bd6e77c170a428da987ad44337ed9ad434eb26
Denys <denisbadakaty51@gmail.com>
2024-04-28 21:33:37 +0300

    add chunks for pages + wrap routes in suspens

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,19 @@
-import classNames from './index.module.scss';
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutLazyPage } from '../../pages/About/index.lazy';
+import { MainLazyPage } from '../../pages/Main/index.lazy';
 
 export const App = () => {
   return (
     <div className="app">
-      <p className={classNames.test}>Root App</p>
-      <p>Hello world</p>
+      <Link to={'/about'}>about</Link>
+      <Link to={'/'}>main</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutLazyPage />} />
+          <Route path="/" element={<MainLazyPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
