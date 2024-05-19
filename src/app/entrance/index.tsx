2edaf1d78252e1f79be1db19c2d51e0b10b96dec
Denys <denisbadakaty51@gmail.com>
2024-05-19 19:28:33 +0300

    update update entrance component

--- src/app/entrance/index.tsx
+++ src/app/entrance/index.tsx
@@ -1,18 +1,15 @@
-import { Link } from 'react-router-dom';
 import { useTheme } from '../providers';
-import { buildClassNames } from 'shared';
+import { useClassNames } from 'shared';
 import { Routes } from '../lib';
+import { NavBar } from 'widgets';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div
-      className={buildClassNames({ classNames: 'app', additional: [theme] })}
-    >
+    <div className={useClassNames({ classNames: 'app', additional: [theme] })}>
+      <NavBar />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/about'}>about</Link>
-      <Link to={'/'}>main</Link>
       <Routes />
     </div>
   );
