732548e609c1600cc5490ffec7a1a768e428beef
Denys Badaka <denys.badaka@sysgears.com>
2024-08-02 15:52:41 +0300

    update scss file names

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'shared';
 import { App } from './entrance';
-import './styles/index.scss';
+import './styles/index.main.scss';
 
 const rootElement = document.getElementById('root');
 
