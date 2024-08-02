594ca4cf5fbaf89f5e424f029a04225b166ec3fb
Denys Badaka <denys.badaka@sysgears.com>
2024-08-02 15:32:05 +0300

    update global.d.ts

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,12 +1,3 @@
-declare module '*.module.scss';
-declare module '*.scss' {
-  interface IClassNames {
-    [className: string]: string;
-  }
-  const classNames: IClassNames;
-  export = classNames;
-}
-
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
