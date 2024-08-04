39b76d938a570decadb4aa393f07f108a66bc895
Denys Badaka <denys.badaka@sysgears.com>
2024-08-04 19:04:42 +0300

    update sidebar

--- src/widgets/Sidebar/ui/components/Switchers/ui/index.tsx
+++ src/widgets/Sidebar/ui/components/Switchers/ui/index.tsx
@@ -1,10 +1,11 @@
-import { ThemeSwitcher } from 'features';
+import { ThemeSwitcher, LanguageSwitcher } from 'features';
 import classNames from './index.module.scss';
 
 export const Switchers = () => {
   return (
     <div className={classNames.container}>
       <ThemeSwitcher />
+      <LanguageSwitcher className={classNames.language} />
     </div>
   );
 };
