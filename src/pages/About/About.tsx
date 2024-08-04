fe06d2bbdd64c389eb8aabb6492851a34285018f
Denys Badaka <denys.badaka@sysgears.com>
2024-08-04 19:05:54 +0300

    update About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,10 @@
+import { useTranslation } from 'react-i18next';
+import { TranslationChunks } from 'shared';
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  const { t } = useTranslation(TranslationChunks.ABOUT);
+
+  return <div>{t('About us')}</div>;
 };
 
 export default AboutPage;
