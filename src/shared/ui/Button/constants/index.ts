68c07e2c6e576d7a1bbc326495c3db28814c9733
Denys Badaka <denys.badaka@sysgears.com>
2024-08-07 00:06:29 +0300

    add stories for button

--- src/shared/ui/Button/constants/index.ts
+++ src/shared/ui/Button/constants/index.ts
@@ -1,3 +1,5 @@
 export enum ButtonVariant {
   CLEAR = 'clear',
+  PRIMARY = 'primary',
+  OUTLINE = 'outline',
 }
---
+++ src/shared/ui/Button/ui/button.stories.tsx
@@ -0,0 +1,42 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/configs';
+import { ThemeVariants } from 'shared/providers';
+
+import { ButtonVariant } from '../constants';
+
+import { Button } from '.';
+
+export default {
+  title: 'shared/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: 'Button text',
+  variant: ButtonVariant.PRIMARY,
+};
+
+export const Clear = Template.bind({});
+Clear.args = {
+  children: 'Button text',
+  variant: ButtonVariant.CLEAR,
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+  children: 'Button text',
+  variant: ButtonVariant.OUTLINE,
+};
+
+export const OutlineDark = Template.bind({});
+OutlineDark.args = {
+  children: 'Button text',
+  variant: ButtonVariant.OUTLINE,
+};
+OutlineDark.decorators = [ThemeDecorator(ThemeVariants.DARK)];
