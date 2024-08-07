09d426565cbbbb4e8a0b88bd158016f0da630ecb
Denys Badaka <denys.badaka@sysgears.com>
2024-08-07 13:28:18 +0300

    add link stories

---
+++ src/shared/ui/Link/ui/appLink.stories.tsx
@@ -0,0 +1,46 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/configs';
+import { ThemeVariants } from 'shared/providers';
+
+import { AppLinkTheme } from '../constants';
+
+import { Link } from '.';
+
+export default {
+  title: 'shared/AppLink',
+  component: Link,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: {
+    to: '/',
+  },
+} as ComponentMeta<typeof Link>;
+
+const Template: ComponentStory<typeof Link> = (args) => <Link {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: 'Text',
+  theme: AppLinkTheme.PRIMARY,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Text',
+  theme: AppLinkTheme.SECONDARY,
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+  children: 'Text',
+  theme: AppLinkTheme.PRIMARY,
+};
+PrimaryDark.decorators = [ThemeDecorator(ThemeVariants.DARK)];
+
+export const SecondaryDark = Template.bind({});
+SecondaryDark.args = {
+  children: 'Text',
+  theme: AppLinkTheme.SECONDARY,
+};
+SecondaryDark.decorators = [ThemeDecorator(ThemeVariants.DARK)];
