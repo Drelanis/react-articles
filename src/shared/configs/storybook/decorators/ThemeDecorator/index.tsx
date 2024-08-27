dfe820e29c6a4aa0583eb00ee6a0846787957ae5
Denys Badaka <denys.badaka@sysgears.com>
2024-08-27 17:19:55 +0300

    update ThemeProvider

--- src/shared/configs/storybook/decorators/ThemeDecorator/index.tsx
+++ src/shared/configs/storybook/decorators/ThemeDecorator/index.tsx
@@ -1,12 +1,14 @@
 import { Story } from '@storybook/react';
 
-import { ThemeVariants } from '$shared/providers';
+import { ThemeProvider, ThemeVariants } from '$shared/providers';
 
 export const ThemeDecorator =
   (theme: ThemeVariants) => (StoryComponent: Story) => {
     return (
-      <div className={`app ${theme}`}>
-        <StoryComponent />
-      </div>
+      <ThemeProvider initialTheme={theme}>
+        <div className={`app ${theme}`}>
+          <StoryComponent />
+        </div>
+      </ThemeProvider>
     );
   };
