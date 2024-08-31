dc6f19fbcd680f92caa36064304d85ec0880e86a
Denys Badaka <denys.badaka@sysgears.com>
2024-08-31 13:21:22 +0300

    minor fix

--- src/pages/Main/ui/MainPage.tsx
+++ src/pages/Main/ui/MainPage.tsx
@@ -1,18 +1,12 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Counter } from '$entities';
 import { TranslationChunks } from '$shared';
 
 const MainPage: FC = () => {
   const { t } = useTranslation(TranslationChunks.MAIN);
 
-  return (
-    <div>
-      {t('mainPage')}
-      <Counter />
-    </div>
-  );
+  return <div>{t('mainPage')}</div>;
 };
 
 export default MainPage;
