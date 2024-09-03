db821a1ba28f2aecf29c0077c4272e97b3b89a15
Denys Badaka <denys.badaka@sysgears.com>
2024-09-03 21:40:01 +0300

    minor fix

--- src/features/Logout/model/index.ts
+++ src/features/Logout/model/index.ts
@@ -1,14 +1,18 @@
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { userActions } from '$entities';
-import { useAppDispatch } from '$shared';
+import { AppRoutes, useAppDispatch } from '$shared';
 
 export const useModel = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const onLogout = useCallback(() => {
     dispatch(userActions.logout());
-  }, [dispatch]);
+
+    navigate(AppRoutes.ABOUT);
+  }, [dispatch, navigate]);
 
   return { onLogout };
 };
