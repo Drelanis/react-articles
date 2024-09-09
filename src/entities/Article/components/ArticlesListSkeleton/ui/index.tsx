82f9e792c0d15da3cf85946f2351ee9d36c7f166
Denys Badaka <denys.badaka@sysgears.com>
2024-09-09 12:50:35 +0300

    update styles

--- src/entities/Article/components/ArticlesListSkeleton/ui/index.tsx
+++ src/entities/Article/components/ArticlesListSkeleton/ui/index.tsx
@@ -17,35 +17,31 @@ export const ArticlesListSkeleton: FC<Props> = memo((props: Props) => {
 
   if (view === ArticleView.LIST) {
     return (
-      <div className={containerClassNames}>
-        <Card>
-          <div className={classNames.header}>
-            <Skeleton border="50%" height={30} width={30} />
-            <Skeleton width={150} height={16} className={classNames.username} />
-            <Skeleton width={150} height={16} className={classNames.date} />
-          </div>
-          <Skeleton width={250} height={24} className={classNames.title} />
-          <Skeleton height={200} className={classNames.img} />
-          <div className={classNames.footer}>
-            <Skeleton height={36} width={200} />
-          </div>
-        </Card>
-      </div>
+      <Card className={containerClassNames}>
+        <div className={classNames.header}>
+          <Skeleton border="50%" height={30} width={30} />
+          <Skeleton width={150} height={16} className={classNames.username} />
+          <Skeleton width={150} height={16} className={classNames.date} />
+        </div>
+        <Skeleton width={250} height={24} className={classNames.title} />
+        <Skeleton height={200} className={classNames.img} />
+        <div className={classNames.footer}>
+          <Skeleton height={36} width={200} />
+        </div>
+      </Card>
     );
   }
 
   return (
-    <div className={containerClassNames}>
-      <Card>
-        <div className={classNames.imageWrapper}>
-          <Skeleton width={200} height={200} className={classNames.img} />
-        </div>
-        <div className={classNames.infoWrapper}>
-          <Skeleton width={130} height={16} />
-        </div>
-        <Skeleton width={150} height={16} className={classNames.title} />
-      </Card>
-    </div>
+    <Card className={containerClassNames}>
+      <div className={classNames.imageWrapper}>
+        <Skeleton width="100%" height={200} className={classNames.img} />
+      </div>
+      <div className={classNames.infoWrapper}>
+        <Skeleton width={130} height={16} />
+      </div>
+      <Skeleton width={150} height={16} className={classNames.title} />
+    </Card>
   );
 });
 
