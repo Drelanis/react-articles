1dfbc7cb3c7215b48b1b5b10de952d45b67826bf
Denys Badaka <denys.badaka@sysgears.com>
2024-09-10 11:39:09 +0300

    add OptionalType

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -13,3 +13,8 @@ declare module '*.svg' {
 declare const IS_DEV: boolean;
 declare const API: string;
 declare const PROJECT: 'storybook' | 'jest' | 'frontend';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- exception
+type OptionalRecord<K extends keyof any, T> = {
+  [P in K]?: T;
+};
