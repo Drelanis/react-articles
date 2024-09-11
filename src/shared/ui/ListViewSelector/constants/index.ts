dbf3fcb59023c116273e86aceac31e6fd7bbf538
Denys Badaka <denys.badaka@sysgears.com>
2024-09-11 15:02:35 +0300

    fix unit tests

--- src/shared/ui/ListViewSelector/constants/index.ts
+++ src/shared/ui/ListViewSelector/constants/index.ts
@@ -1,6 +1,7 @@
+import { ListView } from '../../../constants';
+
 import ListIcon from '$shared/assets/icons/list-24-24.svg';
 import TiledIcon from '$shared/assets/icons/tiled-24-24.svg';
-import { ListView } from '$shared/constants';
 
 export const viewTypes = [
   {
