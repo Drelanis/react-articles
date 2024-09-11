89603c4f266f5fc49fa8b9b6678f6ce98934623b
Denys Badaka <denys.badaka@sysgears.com>
2024-09-11 19:43:58 +0300

    minor fix

--- src/shared/constants/common/index.ts
+++ src/shared/constants/common/index.ts
@@ -1,4 +1,4 @@
 export const ARTICLES_LIST_ITEMS_LIMIT = 4;
-export const ARTICLES_TILE_ITEMS_LIMIT = 9;
+export const ARTICLES_TILE_ITEMS_LIMIT = 10;
 export const SCROLL_SAVE_DELAY = 500;
 export const FETCH_DATA_DELAY = 500;
