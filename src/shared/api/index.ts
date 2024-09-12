20af67a005464490c3caf0ffadde0419cd8e8d7c
Denys Badaka <denys.badaka@sysgears.com>
2024-09-12 17:18:54 +0300

    add interceptors

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -4,7 +4,17 @@ import { USER_LOCAL_STORAGE_KEY } from '$shared/constants';
 
 export const $api = axios.create({
   baseURL: API,
-  headers: {
-    Authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  const newConfig = {
+    ...config,
+  };
+
+  if (newConfig.headers) {
+    newConfig.headers.Authorization =
+      localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+  }
+
+  return newConfig;
 });
