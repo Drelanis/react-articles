1a31ba39ebf54091f0386ef6b3c2360e9756aaa3
Denys Badaka <denys.badaka@sysgears.com>
2024-09-16 15:26:46 +0300

    update componentTemplate

--- scripts/createFsdSlice/templates/componentTemplate.js
+++ scripts/createFsdSlice/templates/componentTemplate.js
@@ -4,7 +4,7 @@ module.exports = (componentName) => `
 import { buildClassNames } from '$shared';
 import { useTranslation } from 'react-i18next';
 import classNames from './index.module.scss';
-import { memo } from 'react';
+import { FC, memo } from 'react';
 
 ${typeConst} Props = {
     className?: string;
@@ -14,7 +14,7 @@ export const ${componentName}: FC<Props> = memo((props) => {
     const { className } = props;
     const { t } = useTranslation();
 
-    const { containerClassNames } = useStyles({ classNames })
+    const { containerClassNames } = useStyles({ className })
     
     return (
         <div className={containerClassNames}>
